3d2fc42f6611318bc795faccf62c4924fb8e9b19
alansastre <alan@alansastre.co>
2022-02-24 12:15:04 +0100

    commit3

---
+++ src/test/java/com/example/SuiteAll.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.example.behavioral", "com.example.creational", "com.example.structural"})
+public class SuiteAll {
+}
--- src/test/java/com/example/structural/MainTest.java
+++ src/test/java/com/example/structural/MainTest.java
@@ -1,4 +1,11 @@
 package com.example.structural;
 
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    void name() {
+        System.out.println("hi");
+    }
 }
