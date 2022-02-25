9f7489656ff97a9dc307119b52fc05d9bc0fb81d
alansastre <alan@alansastre.co>
2022-02-25 09:14:33 +0100

    removed mains

--- src/main/java/com/example/creational/Main.java
+++
@@ -1,4 +0,0 @@
-package com.example.creational;
-
-public class Main {
-}
--- src/test/java/com/example/SuiteAll.java
+++ src/test/java/com/example/SuiteAll.java
@@ -4,6 +4,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages({"com.example.behavioral", "com.example.creational", "com.example.structural"})
+@SelectPackages({"com.example.behavioral", "com.example.creational",
+        "com.example.factory", "com.example.structural"})
 public class SuiteAll {
 }
